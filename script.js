bfd2292340fa4cacf43fa8b33d8b4a5886b3abaf
Ilya <ilya070105@gmail.com>
2024-07-23 15:25:07 +0300

    added optimization for questions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+ "use strict";
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
 console.log(numberOfFilms);12
@@ -10,15 +10,34 @@ const personalMovieDB = {
 	genres: [],
 	privat: false
 };
+for (let i = 0; i < numberOfFilms; i++){
+	const a = prompt('Один из последних просмотренных фильмов?',''),
+			b = prompt('На сколько оцените его??','');
+	if (a != null && b != null && a.length != 50 && a != '' && b != ''){
+		personalMovieDB.movies[a]= b;
+		console.log('done');
+	}
+	else{
+		console.log('error');
+		i--;
+	}
+}
+if (personalMovieDB.count < 10){
+	console.log("Просмотрено довольно мало фильмов")
 
-let a = prompt('Один из последних просмотренных фильмов?','');
-let b = prompt('На сколько оцените его??','');
-let c = prompt('Один из последних просмотренных фильмов?','');
-let d = prompt('На сколько оцените его??','');
+}
+else if
+	(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+		console.log("Вы классический зритель")
+	}
+else if (personalMovieDB.count >= 30){
+	console.log("Вы киноман")
+}
+else{
+	console.log("Произошла ошибка");
+};
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
 
 
